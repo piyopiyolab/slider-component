88fbbbd09aef2e7d8152fdf48634ffc53a90c75a
Marion <piouceau.marion@gmail.com>
2024-04-05 15:52:29 +0200

    -css slider

--- 1.slider/src/Components/Slider/Slider.jsx
+++ 1.slider/src/Components/Slider/Slider.jsx
@@ -1,21 +1,42 @@
 import './Slider.css'
 import leftChevron from "../../assets/left-arrow.svg"
 import rightChevron from "../../assets/right-arrow.svg"
-function Slider() {
+import sliderData from '../../data/sliderData'
+
+
+
+function Slider({ data }) {
+
+    data = sliderData
+
+
+
+
+
     return (
         <>
 
+
+
             <p className="index-info">3 / 5</p>
             <div className="slider">
-                <p className="image-info">image info</p>
+
+
+
+                <p className="img-info">image info</p>
                 <img src="/images/img-3.jpg" alt="" className="slider-img" />
+
+                <button className="navigation-button prev-button">
+                    <img src={leftChevron} alt="left icon" />
+                </button>
+
+                <button className="navigation-button next-button">
+                    <img src={rightChevron} alt="right icon" />
+                </button>
+
+
             </div>
-            <button className="navigation-button prev-button">
-                <img src={leftChevron} alt="left icon" />
-            </button>
-            <button className="navigation-button next-button">
-                <img src={rightChevron} alt="right icon" />
-            </button>
+
         </>
     )
 }
